d49b5a0a59f187a209ee2a43a61f269df4973036
“psirusteam” <psirusteam@gmail.com>
2017-08-23 15:12:16 -0500

    Add a new function - PikSTPPS Add a the README file

---
+++ R/PikSTPPS.R
@@ -0,0 +1,94 @@
+#' @import TeachingSampling
+#' @export
+#' 
+#' @title
+#' Inclusion Probabilities in Stratified Proportional to Size Sampling Designs
+#' @description 
+#' For a given sample size, in each stratum, this function returns a vector of first order
+#' inclusion probabilities for an stratified sampling design proportional to an auxiliary variable. 
+#' @return 
+#' A vector of inclusion probablilities in a stratified finite population.
+#' @details
+#' Consider a population \eqn{U} divide in \eqn{H} strata. within stratum \eqn{h} (\eqn{h=1, \ldots, H}),
+#' for a given vector of auxiliary information with value \eqn{x_k} for the \eqn{k}-th unit and 
+#' population total \eqn{t_{xh}}, the following expression 
+#' \deqn{\pi_k=n_h\times \frac{x_k}{t_{xh}}
+#' is not always less than unity. A sequential algorithm must be used in order to ensure that for 
+#' every unit in the population the inclusion probability gives a proper value; i.e. less or equal
+#' to unity.
+#' @author Hugo Andres Gutierrez Rojas <hugogutierrez at usantotomas.edu.co>
+#' @param S Vector identifying the membership to the strata of each unit in the population.
+#' @param x Vector of auxiliary information for each unit in the population.
+#' @param nh The vector defningn the sample size in each stratum.
+#' 
+#' @references 
+#' Gutierrez, H. A. (2009), \emph{Estrategias de muestreo: Diseno de encuestas y estimacion de parametros}. Editorial Universidad Santo Tomas
+#' Sarndal, C-E. and Swensson, B. and Wretman, J. (2003), \emph{Model Assisted Survey Sampling}. Springer.
+#' @seealso \code{\link{PikHol}, \link{PikPPS}, \link{S.STpiPS}}
+#' @examples 
+#' ############
+#' ## Example 1
+#' ############
+#' # Vector U contains the label of a population of size N=5
+#' U <- c("Yves", "Ken", "Erik", "Sharon", "Leslie")
+#' # The auxiliary information
+#' x <- c(52, 60, 75, 100, 50)
+#' # Vector Strata contains an indicator variable of stratum membership
+#' Strata <- c("A", "A", "A", "B", "B")
+#' # The sample size in each stratum
+#' nh <- c(2,2)
+#' # The vector of inclusion probablities for a stratified piPS sample 
+#' # without replacement of size two within each stratum
+#' Pik <- PikSTPPS(Strata, x, nh)
+#' Pik
+#' 
+#' # Some checks
+#' sum(Pik)
+#' sum(nh)
+#' 
+#' ############
+#' ## Example 2
+#' ############
+#' # Uses the Lucy data to compute the vector of inclusion probablities 
+#' # for a stratified random sample according to a piPS design in each stratum
+#' 
+#' data(Lucy)
+#' attach(Lucy)
+#' # Level is the stratifying variable
+#' summary(Level)
+#' 
+#' # Defines the size of each stratum
+#' N1<-summary(Level)[[1]]
+#' N2<-summary(Level)[[2]]
+#' N3<-summary(Level)[[3]]
+#' N1;N2;N3
+#' 
+#' # Defines the sample size at each stratum
+#' n1<-70
+#' n2<-100
+#' n3<-200
+#' nh<-c(n1,n2,n3)
+#' nh
+#' 
+#' # Computes the inclusion probabilities for the stratified population
+#' S <- Level
+#' x <- Employees
+#' Pik <- PikSTPPS(S, x, nh)
+#' 
+#' # Some checks
+#' sum(Pik)
+#' sum(nh)
+
+PikSTPPS <- function(S, x, nh){
+  S <- as.factor(S)
+  S <- as.factor(as.integer(S))
+  pik <- matrix(NA, nrow = length(x), ncol = 1)
+  cum <- cumsum(nh)
+  
+  for(k in 1:length(nh)){
+    h <- which(S == k)
+    pik.h <- PikPPS(nh[k], x[h])
+    pik[h, ] <- pik.h 
+  }
+  pik
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+# TeachingSampling
+### An R package that drwas complex samples and estimates complex parameters
+
+This is the version control site for the `TeachingSampling` package. This software allows you to select samples from the most common (but somehow complex) sampling schemes. Along with this feature, the software allows you to estimate parameters such as totals, means, ratios, coefficient regressions, percentiles, medians, etc. 
+
+The software is based on the book written by the author of the package.
+
+Gutierrez, H. A. (2009), \emph{Estrategias de muestreo: Diseno de encuestas y estimacion de parametros}. Editorial Universidad Santo Tomas
+
+## Installation
+
+First, you need to install the `devtools` R package
+```
+install.packages("devtools")
+```
+
+Then load the `devtools` R package
+```
+library(devtools)
+```
+
+Finnaly type
+```
+install_github("psirusteam/TeachingSampling")
+```
+
+## Author
+This package is maintained by Andrés Gutiérrez. Current version includes contributions from Natalia Arteaga.
+
+### Support or Contact
+Having trouble with `samplesize4surveys`? Check out the [documentation](http://cran.r-project.org/web/packages/samplesize4surveys/samplesize4surveys.pdf) or [contact support](https://github.com/psirusteam). Comments, amends, and critics are very welcome. This is the [CRAN site](http://cran.r-project.org/web/packages/samplesize4surveys/samplesize4surveys.pdf) of the last stable version of the package.
