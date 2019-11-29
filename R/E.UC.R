272b48b89ab957f5ce15296321bfc9ef2163251b
psirusteam <psirusteam@gmail.com>
2019-11-29 13:37:16 -0300

    New function E.UC

---
+++ R/E.UC.R
@@ -0,0 +1,139 @@
+#' @importFrom magrittr %>%
+#' @importFrom dplyr group_by
+#' @importFrom dplyr summarise
+#' @importFrom dplyr left_join
+#' @export
+#' 
+#' @title
+#' Estimation of the Population Total and its variance using the Ultimate Cluster technique
+#' @description 
+#' This function computes a weighted estimator of the population total and
+#' estimates its variance by using the Ultimate Cluster technique. This approximation
+#' performs well in many sampling designs. The user specifically needs to
+#' declare the variables of interest, the primary sampling units, the strata, and 
+#' the sampling weights for every singlt unit in the sample. 
+#' @return 
+#' This function returns the estimation of the population total of 
+#' every single variable of interest, its estimated standard error 
+#' and its estimated coefficient of variation.
+#' @details
+#' The function returns a data matrix whose columns correspond to 
+#' the estimated parameters of the variables of interest.
+#' @author Hsugo Andres Gutierrez Rojas <hugogutierrez at gmail.com>
+#' @param S Vector identifying the membership to the strata of each unit in selected sample.
+#' @param PSU Vector identifying the membership to the strata of 
+#' each unit in the population.
+#' @param dk Sampling weights of the units in the sample.
+#' @param y Vector, matrix or data frame containig the recollected 
+#' information of the variables of interest for every unit in the 
+#' selected sample.
+#' 
+#' @references 
+#' Sarndal, C-E. and Swensson, B. and Wretman, J. (1992), \emph{Model Assisted Survey Sampling}. Springer.\cr
+#' Gutierrez, H. A. (2009), \emph{Estrategias de muestreo: Diseno de encuestas y estimacion de parametros}. Editorial Universidad Santo Tomas
+#' 
+#' @seealso \code{\link{E.2SI}}
+#' 
+#' @examples
+#' 
+#' #############################
+#' ## Example 1: Two-stage SI ##
+#' #############################
+#' 
+#' data('BigCity')
+#' FrameI <- BigCity %>% group_by(PSU) %>%
+#' summarise(Stratum = unique(Stratum),
+#'           Persons = n(),
+#'           Income = sum(Income),
+#'           Expenditure = sum(Expenditure))
+#'             
+#' attach(FrameI)
+#' 
+#' sizes = FrameI %>% group_by(Stratum) %>%
+#'         summarise(NIh = n(),
+#'         mIh = 2,
+#'         dI = NIh/mIh)
+#'         
+#' NIh <- sizes$NIh
+#' mIh <- sizes$mIh
+#' 
+#' samI <- S.STSI(Stratum, NIh, mIh)
+#' UI <- levels(as.factor(FrameI$PSU))
+#' sampleI <- UI[samI]
+#'           
+#' FrameII <- left_join(sizes, BigCity[which(BigCity$PSU %in% sampleI), ])
+#' attach(FrameII)
+#' 
+#' Ni <- as.vector(table(FrameII$PSU))
+#' ni <- ceiling(Ni * 0.15)
+#' ni
+#' sum(ni)
+#' 
+#' sam = S.SI(Ni[1], ni[1])
+#' cluster = FrameII[which(FrameII$PSU == sampleI[1]), ]
+#' cluster$dki <- Ni[1]/ni[1]
+#' data = cluster[sam, ]
+#' for (i in 2:length(Ni)) {
+#'       sam = S.SI(Ni[i], ni[i])
+#'       cluster = FrameII[which(FrameII$PSU == sampleI[i]), ]
+#'       cluster$dki <- Ni[i]/ni[i]
+#'       data1 = cluster[sam, ]
+#'       data = rbind(data, data1)
+#' }
+#' 
+#' data$dk <- data$dI * data$dki
+#' sum(data$dk)
+#' attach(data)
+#' estima <- data.frame(Income, Expenditure)
+#' area <- as.factor(PSU)
+#' stratum <- as.factor(Stratum)
+#' 
+#' E.UC(stratum, area, dk, estima)
+
+E.UC <- function(S, PSU, dk, y) {
+  y <- cbind(1, y)
+  y <- as.data.frame(y)
+  names(y)[1] <- "N"
+  PSU <- as.factor(PSU)
+  S <- as.factor(S)
+  
+  Total <- matrix(NA, nrow = 4, ncol = dim(y)[2])
+  rownames(Total) = c("Estimation", "Standard Error", "CVE", 
+                      "DEFF")
+  colnames(Total) <- names(y)
+  
+  for (k in 1:dim(y)[2]) {
+    yk <- tyi <- NULL
+    matriz <- data.frame(y[, k], dk, PSU, S)
+    colnames(matriz)[1] <- "yk"  
+    
+    P1 <- matriz %>% 
+      group_by(PSU) %>%
+      summarise(tyi = sum(yk * dk),
+                S = unique(S))
+    
+    P2 <- P1 %>% 
+      group_by(S) %>%
+      summarise(barty = mean(tyi))
+    
+    P3 <- matriz %>% 
+      group_by(S) %>%
+      summarise(mi = length(unique(PSU)))
+    
+    T1 <- left_join(left_join(P1, P2, by="S"), P3, by="S")
+    T1$s2 <- (T1$mi/(T1$mi-1))*(T1$tyi - T1$barty)^2
+    
+    Vty <- sum(T1$s2, na.rm = T)
+    ty <- sum(matriz$yk * dk)
+    
+    CVe <- 100 * sqrt(Vty)/ty
+    n <- length(y[, k])
+    N <- sum(dk)
+    VMAS <- (N^2) * (1 - (n/N)) * var(y[, k])/(n)
+    DEFF <- Vty/VMAS
+    Total[, k] <- c(ty, sqrt(Vty), CVe, DEFF)
+  }
+  return(Total)
+}
+
+
